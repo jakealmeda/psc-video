acd234c0ae49ac58e33335ebd03a28e9b236ab77
Jake Almeda <jakealmeda@gmail.com>
2021-07-25 13:29:51 +0800

    1.0 | Links Template renamed

--- lib/acf-psc-autofill.php
+++ lib/acf-psc-autofill.php
@@ -23,9 +23,36 @@ function psc_layout_video_select( $field ) {
     
     $file_extn = 'php';
 
-    // get all files found in VIEWS folder
+    // get all files found in VIDEOS folder
     //$view_dir = get_stylesheet_directory().'/partials/views/';
     $view_dir = psc_plugin_dir_path_vids().'partials/videos/';
+
+    $data_from_dir = setup_get_block_views( $view_dir, $file_extn );
+
+    $field['choices'] = array();
+
+    //Loop through whatever data you are using, and assign a key/value
+    if( is_array( $data_from_dir ) ) {
+
+        foreach( $data_from_dir as $field_key => $field_value ) {
+            
+            $field['choices'][$field_key] = $field_value;
+        }
+
+        return $field;
+
+    }
+    
+}
+
+
+add_filter( 'acf/load_field/name=links-layout', 'psc_layout_links_select' );
+function psc_layout_links_select( $field ) {
+    
+    $file_extn = 'php';
+
+    // get all files found in LINKS folder
+    $view_dir = psc_plugin_dir_path_vids().'partials/links/';
 
     $data_from_dir = setup_get_block_views( $view_dir, $file_extn );
 
